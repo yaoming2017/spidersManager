a4e5e2cf9ffe0b7ce35d39bb9e1349a694e68f21
wlw <294645581@qq.com>
2017-05-18 16:41:40 +0800

    数据清洗阶段，第一阶段暂时结束。

---
+++ src/com/sicdlib/controller/GhostController.java
@@ -0,0 +1,17 @@
+package com.sicdlib.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GhostController {
+
+
+    @RequestMapping("ghostDouban")
+    public String index(Model model) {
+
+        return "index";
+    }
+
+}

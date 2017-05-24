fdfe88559954daabaa5ad4f828cd3b3b2217e033
Yh <yanghaov119@126.com>
2017-05-24 10:49:30 +0800

    Merge branch 'master' of github.com:V119/spidersManager
    # Conflicts:
    #	resource/conf.properties
    #	src/com/sicdlib/dao/IStopWordsDAO.java
    #	src/com/sicdlib/dao/imple/StopWordsDAO.java

--- src/com/sicdlib/service/IStopWordsService.java
+++ src/com/sicdlib/service/IStopWordsService.java
@@ -1,10 +1,7 @@
 package com.sicdlib.service;
 
-import com.sicdlib.dto.DoubanGroupPostEntity;
-import com.sicdlib.dto.TbStopWordsEntity;
-
 import java.util.List;
 
 public interface IStopWordsService {
-    public List<TbStopWordsEntity> getAllStopWords();
+    public List<String> getAllStopWords();
 }

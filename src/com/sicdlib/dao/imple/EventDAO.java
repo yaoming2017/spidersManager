0c1e4ff9c1f244064a697dd68d04a10b834f2dee
wlw <294645581@qq.com>
2017-05-31 09:23:50 +0800

    合并

--- src/com/sicdlib/dao/imple/EventDAO.java
+++ src/com/sicdlib/dao/imple/EventDAO.java
@@ -156,7 +156,7 @@ public class EventDAO implements IEventDAO {
 
     @Override
     public TbEventEntity getEventByName(String eventName) {
-        String hql = "from TbEventArticleEntity e where e.eventName = '"+eventName+"'";
+        String hql = "from TbEventEntity e where e.eventName = '"+eventName+"'";
         return (TbEventEntity) baseDAO.get(hql);
     }
 }
